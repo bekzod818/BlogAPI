5c6ef0a6c23d65636d0856d235390c87aeaff735
bekzod818 <bekzod030900@gmail.com>
2021-08-27 22:25:26 +0500

    static & dynamic schemas

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,4 +8,9 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')),
     path('api/v1/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('openapi/', get_schema_view(
+        title='Blog API',
+        description='A sample API for learning DRF',
+        version="1.0.0"
+    ), name="openapi-schema"),
 ]
